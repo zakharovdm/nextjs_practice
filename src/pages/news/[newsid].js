594937d0e651860efe48d44d4b002d46ff044880
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-05-03 23:36:28 +0300

    Extracting dynamic parameters

--- src/pages/news/[newsid].js
+++ src/pages/news/[newsid].js
@@ -1,3 +1,10 @@
+import { useRouter } from "next/router";
+
 export default function DetailPage() {
+
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
   return <h1>The Detail Page</h1>;
 }
