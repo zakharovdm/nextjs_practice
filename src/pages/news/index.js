994c115e578d1eccdfa072845db6eba5e10a9dca
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-05-03 23:51:20 +0300

    linking between pages

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/news1">news 1</Link>
+        </li>
+        <li>
+          <Link href="/news/news2">news 1</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
